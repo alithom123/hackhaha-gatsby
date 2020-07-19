06401ec559507d61652da1355d9c1799efa33688
Ali Thompson <alithom123@gmail.com>
2020-07-19 15:48:36 -0500

    change type to text

--- src/components/form.js
+++ src/components/form.js
@@ -3,6 +3,7 @@ import React from "react"
 const Form = () => {
   return (
     <div>
+      <h2>Get in Touch</h2>
       <form
         name="contact"
         method="post"
@@ -10,7 +11,7 @@ const Form = () => {
         data-netlify-honeypot="bot-field"
       >
         <input name="name" placeholder="Your Name" type="text" />
-        <input name="email" placeholder="Email" type="email" />
+        <input name="email" placeholder="Email" type="text" />
         <textarea
           name="message"
           placeholder="Let us know if you have any questions."
